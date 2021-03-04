54e35a3b89755b4edb3cd46c87d558ef74ef91e9
mexell96 <mexell96@gmail.com>
2021-03-04 22:23:08 +0300

    filled Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,90 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { GithubContext } from "../context/github/githubContext";
 
-export const Profile = () => {
+export const Profile = ({ match }) => {
+  const { getUser, getRepos, loading, user, repos } = useContext(GithubContext);
+  const urlName = match.params.name;
+
+  useEffect(() => {
+    getUser(urlName);
+    getRepos(urlName);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return <p className="text-center">загрузка...</p>;
+  }
+
+  const {
+    name,
+    company,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+  } = user;
   return (
-    <div>
-      <h1>Profile page</h1>
-    </div>
+    <>
+      <Link to="/" className="btn btn-link">
+        На главную
+      </Link>
+      <div className="card mb-4">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-3 text-center">
+              <img src={avatar_url} alt={name} style={{ width: "150px" }} />
+              <h1>{name}</h1>
+              {location && <p>Местоположение: {location}</p>}
+            </div>
+            <div className="col">
+              {bio && (
+                <>
+                  <h3>BIO</h3>
+                  <p>{bio}</p>
+                </>
+              )}
+              <a
+                href={html_url}
+                className="btn btn-dark"
+                target="_blank"
+                rel="noreferrer">
+                Открыть профиль
+              </a>
+              <ul>
+                {login && (
+                  <li>
+                    <strong>Username: </strong> {login}
+                  </li>
+                )}
+                {company && (
+                  <li>
+                    <strong>Компания: </strong> {company}
+                  </li>
+                )}
+                {blog && (
+                  <li>
+                    <strong>Website: </strong> {blog}
+                  </li>
+                )}
+              </ul>
+              <div className="badge badge-primary">Подписчики: {followers}</div>
+              <div className="badge badge-success">Пописан: {following}</div>
+              <div className="badge badge-info">
+                Репозитории: {public_repos}
+              </div>
+              <div className="badge badge-dark">Gists: {public_gists}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {repos.join()}
+    </>
   );
 };
