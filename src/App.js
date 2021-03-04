0b81f013c1adcff6e9db6d7a90397c80741f9e76
mexell96 <mexell96@gmail.com>
2021-03-04 20:07:40 +0300

    added content in pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { Home } from "./pages/Home";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <div className="container pt-4">
-        <Home />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/profile/:name" component={Profile} />
+        </Switch>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export const Navbar = () => {
   return (
@@ -6,14 +7,14 @@ export const Navbar = () => {
       <div className="navbar-brand">Github поиск</div>
       <ul className="navbar-nav">
         <li className="nav-item">
-          <a href="/" className="nav-link">
+          <NavLink exact to="/" className="nav-link">
             Главная
-          </a>
+          </NavLink>
         </li>
         <li className="nav-item">
-          <a href="/about" className="nav-link">
+          <NavLink to="/about" className="nav-link">
             Информация
-          </a>
+          </NavLink>
         </li>
       </ul>
     </nav>
